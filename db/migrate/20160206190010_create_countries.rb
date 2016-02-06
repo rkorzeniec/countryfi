60bb5eb4af3c5dba8f46353d06f3d893da09ea33
Robert Korzeniec <r.korzeniec@gmail.com>
2016-02-06 21:50:23 +0100

    countries migration

---
+++ db/migrate/20160206190010_create_countries.rb
@@ -0,0 +1,32 @@
+class CreateCountries < ActiveRecord::Migration
+  def up
+    create_table :countries do |t|
+      t.string :name_common
+      t.string :name_official
+      t.text :name_native
+      t.string :tld
+      t.string :cca2
+      t.string :ccn3
+      t.string :cca3
+      t.string :cioc
+      t.string :currency
+      t.string :capital
+      t.text :altSpellings
+      t.string :region
+      t.string :subregion
+      t.text :languages
+      t.text :translations
+      t.string :latlng
+      t.string :demonym
+      t.boolean :landlocked
+      t.text :borders
+      t.float :area
+
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :countries
+  end
+end
