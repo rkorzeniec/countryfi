cc6c336f6438904e9a271001ae261ba7c12c35fb
Robert Korzeniec <r.korzeniec@gmail.com>
2016-02-06 19:57:51 +0100

    countries seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+json = ActiveSupport::JSON.decode(File.read('db/seeds/countries.json'))
+
+json.each do |a|
+  Country.create!(a['country'], without_protection: true)
+end
