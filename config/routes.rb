43f8c43bdd8dbc98bd3c80185bc894ae88622571
Robert Korzeniec <r.korzeniec@gmail.com>
2016-02-07 13:45:23 +0100

    checkins routing added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :countries, only: [:index, :show]
+  resources :checkins, only: [:index, :show, :new, :create]
   get 'about' => 'static_pages#about'
   root 'static_pages#home'
 end
