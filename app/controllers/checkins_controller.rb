1aa78e610a358cb2a5a36c8acfda8e6fadd1a275
Robert Korzeniec <r.korzeniec@gmail.com>
2016-02-20 14:33:48 +0100

    checkins create

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -15,6 +15,15 @@ class CheckinsController < ApplicationController
   end
 
   def create
+    checkin = CheckinCreator.new(current_user, checkin_params)
+
+    respond_to do |format|
+      if checkin.process
+        format.html { redirect_to root_url, notice: 'Checkin done...' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -23,4 +32,10 @@ class CheckinsController < ApplicationController
   def update
 
   end
+
+  private
+
+  def checkin_params
+      params.permit(:country)
+    end
 end
