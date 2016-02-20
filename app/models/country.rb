5691db42982abcb9b1b99330b965000382587221
Robert Korzeniec <r.korzeniec@gmail.com>
2016-02-20 14:34:12 +0100

    find country by any name

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,15 @@
 class Country < ActiveRecord::Base
   has_many :checkins
+
+  def self.find_by_any(name)
+    where(
+      "name_common LIKE ?
+      OR name_official LIKE ?
+      OR cca2 LIKE ?
+      OR ccn3 LIKE ?
+      OR cca3 LIKE ?
+      OR cioc LIKE ?",
+      "%#{name}%", "%#{name}%", "%#{name}%",
+      "%#{name}%", "%#{name}%", "%#{name}%").first
+  end
 end
