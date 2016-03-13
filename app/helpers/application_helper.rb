2c628ec142c857788cddda4ace2c871d8d2bdc41
Robert Korzeniec <r.korzeniec@gmail.com>
2016-03-13 22:31:41 +0100

    change in title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = 'Country Diary'
+    base_title = 'Countryfier'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
