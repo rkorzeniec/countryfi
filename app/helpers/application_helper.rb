1a17479d51dab0619318422b6942fe5ce4e83c65
Robert Korzeniec <r.korzeniec@gmail.com>
2016-09-01 22:35:19 +0200

    render date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = 'Countryfier'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def render_date(date)
+    h(date.strftime('%Y-%m-%d')) if date
+  end
 end
