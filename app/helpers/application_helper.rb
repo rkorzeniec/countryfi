054bab205bd09667d0d2f48c4db196d32028ea52
Robert Korzeniec <r.korzeniec@gmail.com>
2016-10-05 11:52:12 +0200

    svg helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
   def render_date(date)
     h(date.strftime('%Y-%m-%d')) if date
   end
+
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename).source.force_encoding('UTF-8')
+    doc = Nokogiri::HTML::DocumentFragment.parse(file)
+    svg = doc.at_css('svg')
+    svg['class'] = options[:class] if options[:class].present?
+    raw(doc)
+  end
 end
