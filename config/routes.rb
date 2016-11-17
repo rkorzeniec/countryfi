ba8fa1bee0635991208009b7013c1cea9597b956
Robert Korzeniec <r.korzeniec@gmail.com>
2016-11-17 23:29:16 +0100

    removed checkins#show,

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :countries, only: [:show]
-  resources :checkins
+  resources :checkins, except: [:show]
   get 'about' => 'static_pages#about'
   root 'static_pages#home'
 end
