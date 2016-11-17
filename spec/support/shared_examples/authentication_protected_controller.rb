79530267b0de995ec15435090886c36e102f6dfb
Robert Korzeniec <r.korzeniec@gmail.com>
2016-11-17 23:31:16 +0100

    DRYied

--- spec/support/shared_examples/authentication_protected_controller.rb
+++ spec/support/shared_examples/authentication_protected_controller.rb
@@ -1,12 +1,10 @@
-#request[0] method (:get, :post...)
-#request[1] action (:create, :index ...)
-#request[2] params (e.g. {:id=>"1"})
-shared_examples "an authentication protected controller" do |requests|
-  context "a not signed in user" do
+shared_examples 'authentication_protected_controller' do |requests|
+  context 'when user notsigned in user' do
     requests.each do |request|
       it "has no access to #{request[0].upcase}##{request[1]}" do
         send(request[0], request[1], request[2])
         expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
