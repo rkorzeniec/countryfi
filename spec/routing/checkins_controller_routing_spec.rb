877849c7bcaa527898c6bb17da257bc994a4d80d
Robert Korzeniec <r.korzeniec@gmail.com>
2016-11-20 21:07:14 +0100

    removing show route test

--- spec/routing/checkins_controller_routing_spec.rb
+++ spec/routing/checkins_controller_routing_spec.rb
@@ -1,6 +1,5 @@
 describe CheckinsController do
   it { expect(get: '/checkins').to route_to(controller: 'checkins', action: 'index') }
-  it { expect(get: '/checkins/1').to route_to(controller: 'checkins', action: 'show', id: '1') }
   it { expect(get: '/checkins/new').to route_to(controller: 'checkins', action: 'new') }
   it { expect(post: '/checkins').to route_to(controller: 'checkins', action: 'create') }
   it { expect(get: '/checkins/1/edit').to route_to(controller: 'checkins', action: 'edit', id: '1') }
