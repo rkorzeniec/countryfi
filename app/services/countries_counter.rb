4eb1cd2383091d4170cb65414b91f9f9411b0557
Robert Korzeniec <r.korzeniec@gmail.com>
2016-11-21 23:19:16 +0100

    updated functionality with specs

--- app/services/countries_counter.rb
+++ app/services/countries_counter.rb
@@ -1,10 +1,52 @@
 class CountriesCounter
-  def self.european_countries_count
-    Country.where(region: 'Europe').count
+  attr_reader :user
+
+  class << self
+    def european_countries_count
+      Country.where(region: 'Europe').count
+    end
+
+    def north_american_countries_count
+      Country.where([
+        "region = 'Americas' AND "\
+        "subregion = :north_america OR "\
+        "subregion = :central_america OR "\
+        "subregion = :caribbean",
+        {
+          north_america: 'North America',
+          central_america: 'Central America',
+          caribbean: 'Caribbean'
+        }
+      ]).count
+    end
+
+    def south_american_countries_count
+      Country.where(subregion: 'South America').count
+    end
+
+    def asian_countries_count
+      Country.where(region: 'Asia').count
+    end
+
+    def oceanian_countries_count
+      Country.where(region: 'Oceania').count
+    end
+
+    def african_countries_count
+      Country.where(region: 'Africa').count
+    end
   end
 
-  def self.north_american_countries_count
-    Country.where([
+  def initialize(user)
+    @user = user
+  end
+
+  def visited_european_countries_count
+    user.countries.where(region: 'Europe').count
+  end
+
+  def visited_north_american_countries_count
+    user.countries.where([
       "region = 'Americas' AND "\
       "subregion = :north_america OR "\
       "subregion = :central_america OR "\
@@ -17,19 +59,19 @@ class CountriesCounter
     ]).count
   end
 
-  def self.south_american_countries_count
-    Country.where(subregion: 'South America').count
+  def visited_south_american_countries_count
+    user.countries.where(subregion: 'South America').count
   end
 
-  def self.asian_countries_count
-    Country.where(region: 'Asia').count
+  def visited_asian_countries_count
+    user.countries.where(region: 'Asia').count
   end
 
-  def self.oceanian_countries_count
-    Country.where(region: 'Oceania').count
+  def visited_oceanian_countries_count
+    user.countries.where(region: 'Oceania').count
   end
 
-  def self.african_countries_count
-    Country.where(region: 'Africa').count
+  def visited_african_countries_count
+    user.countries.where(region: 'Africa').count
   end
 end
--- spec/services/countries_counter_spec.rb
+++ spec/services/countries_counter_spec.rb
@@ -1,64 +1,248 @@
 describe CountriesCounter do
-  let!(:european_country) { create(:country, region: 'Europe') }
-  let!(:asian_country) { create(:country, region: 'Asia') }
-  let!(:oceanian_country) { create(:country, region: 'Oceania') }
-  let!(:african_country) { create(:country, region: 'Africa') }
-  let!(:north_american_country) do
+  let(:european_country) { create(:country, region: 'Europe') }
+  let(:asian_country) { create(:country, region: 'Asia') }
+  let(:oceanian_country) { create(:country, region: 'Oceania') }
+  let(:african_country) { create(:country, region: 'Africa') }
+  let(:north_american_country) do
     create(:country, region: 'Americas', subregion: 'North America')
   end
-  let!(:south_american_country) do
+  let(:central_american_country) do
+    create(:country, region: 'Americas', subregion: 'Central America')
+  end
+  let(:caribbean_country) do
+    create(:country, region: 'Americas', subregion: 'Caribbean')
+  end
+  let(:south_american_country) do
     create(:country, region: 'Americas', subregion: 'South America')
   end
 
-  describe '.european_countries_count' do
-    subject { described_class.european_countries_count }
+  context 'when class methods are called' do
+    describe '.european_countries_count' do
+      subject { described_class.european_countries_count }
 
-    it { expect(subject).to eq(1) }
-  end
+      context 'when european countries exist' do
+        before { european_country }
 
-  describe '.north_american_countries_count' do
-    subject { described_class.north_american_countries_count }
-
-    it { expect(subject).to eq(1) }
-
-    context 'with central american country' do
-      let!(:central_american_country) do
-        create(:country, region: 'Americas', subregion: 'Central America')
+        it { expect(subject).to eq(1) }
       end
 
-      it { expect(subject).to eq(2) }
+      context 'when european countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
     end
 
-    context 'with caribbean country' do
-      let!(:caribbean_country) do
-        create(:country, region: 'Americas', subregion: 'Caribbean')
+    describe '.north_american_countries_count' do
+      subject { described_class.north_american_countries_count }
+
+      context 'when north american countries exist' do
+        before { north_american_country }
+
+        it { expect(subject).to eq(1) }
+
+        context 'with central american country' do
+          before { central_american_country }
+
+          it { expect(subject).to eq(2) }
+        end
+
+        context 'with caribbean country' do
+          before { caribbean_country }
+
+          it { expect(subject).to eq(2) }
+        end
       end
 
-      it { expect(subject).to eq(2) }
+      context 'when north american countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '.south_american_countries_count' do
+      subject { described_class.south_american_countries_count }
+
+      context 'when south american countries exist' do
+        before { south_american_country }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when south american countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '.asian_countries_count' do
+      subject { described_class.asian_countries_count }
+
+      context 'when asian countries exist' do
+        before { asian_country }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when asian countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '.oceanian_countries_count' do
+      subject { described_class.oceanian_countries_count }
+
+      context 'when oceanian countries exist' do
+        before { oceanian_country }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when oceanian countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '.african_countries_count' do
+      subject { described_class.african_countries_count }
+
+      context 'when african countries exist' do
+        before { african_country }
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when african countries does not exist' do
+        it { expect(subject).to eq(0) }
+      end
     end
   end
 
-  describe '.south_american_countries_count' do
-    subject { described_class.south_american_countries_count }
+  context 'when instance methods are called' do
+    let(:counter) { described_class.new(user) }
+    let(:user) { create(:user) }
+    let(:european_checkin) do
+      create(:checkin, user: user, country: european_country)
+    end
+    let(:north_american_checkin) do
+      create(:checkin, user: user, country: north_american_country)
+    end
+    let(:south_american_checkin) do
+      create(:checkin, user: user, country: south_american_country)
+    end
+    let(:asian_checkin) do
+      create(:checkin, user: user, country: asian_country)
+    end
+    let(:african_checkin) do
+      create(:checkin, user: user, country: african_country)
+    end
+    let(:oceanian_checkin) do
+      create(:checkin, user: user, country: oceanian_country)
+    end
 
-    it { expect(subject).to eq(1) }
-  end
+    describe '#visited_european_countries_count' do
+      subject { counter.visited_european_countries_count }
 
-  describe '.asian_countries_count' do
-    subject { described_class.asian_countries_count }
+      context 'when european checkin exist' do
+        before { european_checkin }
 
-    it { expect(subject).to eq(1) }
-  end
+        it { expect(subject).to eq(1) }
+      end
 
-  describe '.oceanian_countries_count' do
-    subject { described_class.oceanian_countries_count }
+      context 'when european checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
 
-    it { expect(subject).to eq(1) }
-  end
+    describe '#visited_north_american_countries_count' do
+      subject { counter.visited_north_american_countries_count }
 
-  describe '.african_countries_count' do
-    subject { described_class.african_countries_count }
+      context 'when north american checkin exist' do
+        before { north_american_checkin }
 
-    it { expect(subject).to eq(1) }
+        it { expect(subject).to eq(1) }
+
+        context 'with central american country' do
+          let(:central_american_country) do
+            create(:country, region: 'Americas', subregion: 'Central America')
+          end
+          let(:central_american_checkin) do
+            create(:checkin, user: user, country: central_american_country)
+          end
+
+          before { central_american_checkin }
+
+          it { expect(subject).to eq(2) }
+        end
+
+        context 'with caribbean country' do
+          let(:caribbean_country) do
+            create(:country, region: 'Americas', subregion: 'Caribbean')
+          end
+          let(:caribbean_checkin) do
+            create(:checkin, user: user, country: caribbean_country)
+          end
+
+          before { caribbean_checkin }
+
+          it { expect(subject).to eq(2) }
+        end
+      end
+
+      context 'when north american checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '#visited_south_american_countries_count' do
+      subject { counter.visited_south_american_countries_count }
+
+      context 'when south american checkin exist' do
+        before { south_american_checkin }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when south american checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '#visited_asian_countries_count' do
+      subject { counter.visited_asian_countries_count }
+
+      context 'when asian checkin exist' do
+        before { asian_checkin }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when asian checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '#visited_oceanian_countries_count' do
+      subject { counter.visited_oceanian_countries_count }
+
+      context 'when oceanian checkin exist' do
+        before { oceanian_checkin }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when oceanian checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
+
+    describe '#visited_african_countries_count' do
+      subject { counter.visited_african_countries_count }
+
+      context 'when african checkin exist' do
+        before { african_checkin }
+
+        it { expect(subject).to eq(1) }
+      end
+
+      context 'when african checkin does not exist' do
+        it { expect(subject).to eq(0) }
+      end
+    end
   end
 end
