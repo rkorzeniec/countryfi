8454e2b28554e8894a35cc4d89b889f5666bcde2
Robert Korzeniec <r.korzeniec@gmail.com>
2017-01-05 22:38:42 +0100

    skipping authentication for static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+  skip_before_filter :authenticate_user!
 
-  def about
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   resources :checkins, except: [:show]
   resource :dashboard, only: [:show], controller: 'dashboard'
 
-  get 'about' => 'static_pages#about'
   root 'static_pages#home'
 end
