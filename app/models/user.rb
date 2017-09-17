b3633ff525aa7feab1ffc9cabc8280b5271189f9
Robert Korzeniec <r.korzeniec@gmail.com>
2017-09-17 15:52:25 +0200

    shifting things around

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable
-
   has_many :checkins, dependent: :destroy
   has_many :countries, through: :checkins do
     def visited
@@ -11,4 +8,7 @@ class User < ActiveRecord::Base
 
   delegate :european, :north_american, :south_american, :asian, :oceanian,
            :african, :antarctican, to: :countries, prefix: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :trackable, :validatable
 end
