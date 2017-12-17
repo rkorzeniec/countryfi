3b15e37c9640701c5b63e288d83125c4dc590e5e
Robert Korzeniec <r.korzeniec@gmail.com>
2017-12-17 22:22:21 +0100

    country visit badges

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -2,4 +2,21 @@ module CountriesHelper
   def country_code_array(countries)
     countries.map(&:cca2)
   end
+
+  def visit_label(user, country)
+    return unless user.countries.include?(country)
+    label_status = visit_label_status(user, country)
+    label_content = visit_label_content(user, country)
+    content_tag(:span, label_content, class: ['label', label_status])
+  end
+
+  private
+
+  def visit_label_status(user, country)
+    user.visited_countries.include?(country) ? 'label-success' : 'label-info'
+  end
+
+  def visit_label_content(user, country)
+    user.visited_countries.include?(country) ? 'visited' : 'upcoming'
+  end
 end
--- spec/helpers/countries_helper_spec.rb
+++ spec/helpers/countries_helper_spec.rb
@@ -8,4 +8,36 @@ describe CountriesHelper do
 
     it { expect(subject).to eq(['AA', 'BB']) }
   end
+
+  describe '#visit_label' do
+    let(:countries) { [] }
+    let(:visited_countries) { [] }
+    let(:country) { double(:country) }
+    let(:user) do
+      double(:user, countries: countries, visited_countries: visited_countries)
+    end
+
+    subject { helper.visit_label(user, country) }
+
+    context 'when country is not visited nor upcoming' do
+      it { is_expected.to be_nil }
+    end
+
+    context 'when country is visited' do
+      let(:countries) { [country] }
+      let(:visited_countries) { [country] }
+
+      it do
+        is_expected.to eq('<span class="label label-success">visited</span>')
+      end
+    end
+
+    context 'when country is upcoming' do
+      let(:countries) { [country] }
+
+      it do
+        is_expected.to eq('<span class="label label-info">upcoming</span>')
+      end
+    end
+  end
 end
