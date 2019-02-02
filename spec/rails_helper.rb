65acaebab49103beb74d6e6165a15f1ffefb3fc4
rkorzeniec <r.korzeniec@gmail.com>
2019-02-02 19:17:15 +0100

    add shoulda matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,13 @@ end
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.filter_run_excluding migrations: true
