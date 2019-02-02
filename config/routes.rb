ceb03bf310a03d9faea9a47b882fb322c9ab27b4
rkorzeniec <r.korzeniec@gmail.com>
2019-02-02 23:27:32 +0100

    remove exceptions route for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :countries, only: :show do
     member do
       get :geo
     end
   end
+
   resources :checkins, except: %i[index show]
+
   namespace :checkins do
     resources :worlds, only: :index
     resources :europeans, only: :index
@@ -16,7 +19,9 @@ Rails.application.routes.draw do
     resources :oceanians, only: :index
     resources :antarcticans, only: :index
   end
+
   get 'dashboard', action: :index, controller: 'dashboard'
+
   namespace :explore do
     resources :worlds, only: :index
     resources :europes, only: :index
@@ -29,8 +34,4 @@ Rails.application.routes.draw do
   end
 
   root 'home', action: :index, controller: 'home_page'
-
-  unless Rails.application.config.consider_all_requests_local
-    get '*path', to: 'exceptions#show', code: '404'
-  end
 end
