6dfef8dce137373953fea429e3a55a4e9984a262
rkorzeniec <r.korzeniec@gmail.com>
2019-02-02 19:17:31 +0100

    add name delegation in border model

--- app/models/border_country.rb
+++ app/models/border_country.rb
@@ -3,4 +3,6 @@ class BorderCountry < ActiveRecord::Base
   belongs_to :border_country, class_name: 'Country'
 
   validates :border_country, presence: true
+
+  delegate :name_common, to: :border_country
 end
--- spec/models/border_country_spec.rb
+++ spec/models/border_country_spec.rb
@@ -3,4 +3,6 @@ describe BorderCountry do
   it { is_expected.to belong_to(:border_country).class_name('Country') }
 
   it { is_expected.to validate_presence_of(:border_country) }
+
+  it { is_expected.to delegate_method(:name_common).to(:border_country) }
 end
