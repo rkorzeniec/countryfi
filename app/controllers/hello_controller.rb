13e0f86039cb4798a570b5410c8a5a0021feb99b
rkorzeniec <r.korzeniec@gmail.com>
2019-03-16 10:59:07 +0100

    redirect to dashboard when user authenticated

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,13 @@
 class HelloController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :redirect_authenticated
 
   def index; end
+
+  private
+
+  def redirect_authenticated
+    return unless current_user
+    redirect_to dashboard_path
+  end
 end
--- spec/controllers/hello_controller_spec.rb
+++ spec/controllers/hello_controller_spec.rb
@@ -1,12 +1,24 @@
 describe HelloController do
   describe 'GET index' do
     let(:country) { double(:country, id: 1) }
+
     before do
       allow(CountryIDLookuper).to receive(:lookup).and_return(country)
       get(:index)
     end
 
     it { expect(response).to be_successful }
-    it { expect(subject).to render_template('home_page/index') }
+    it { expect(subject).to render_template('hello/index') }
+  end
+
+  context 'when user authenticated' do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in(user)
+      get(:index)
+    end
+
+    it { expect(response).to redirect_to(dashboard_path) }
   end
 end
