1ab90a4427047c9ab692db1bb5e0f3e455b8b0ef
rkorzeniec <r.korzeniec@gmail.com>
2019-03-18 22:48:54 +0100

    update pixeladmin, jquery and fontawesome

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,16 @@ module CountryDiary
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
+    config.assets.precompile += [
+      '*.json',
+      '*.svg',
+      '*.eot',
+      '*.woff',
+      '*.woff2',
+      '*.ttf',
+      '*.png'
+    ]
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
