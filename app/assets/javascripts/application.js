570d5fc28c36808fbe0348adb6940faf8165e4eb
rkorzeniec <r.korzeniec@gmail.com>
2019-03-18 23:04:39 +0100

    fix signout link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require_self
 //= require chartkick
 //= require Chart.bundle
-//= require ./jquery
-//= require ./pixeladmin/bootstrap.min
-//= require ./pixeladmin/pixeladmin.min
-//= require ./pixeladmin/pace.min
-
+//= require jquery
+//= require jquery_ujs
+//= require pixeladmin/bootstrap.min
+//= require pixeladmin/pixeladmin.min
+//= require pixeladmin/pace.min
