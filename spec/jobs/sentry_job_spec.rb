ca295266fd76967308a5674f01632edb5f1c45b0
rkorzeniec <r.korzeniec@gmail.com>
2019-06-15 22:40:05 +0200

    perfom sentry asynchronously

---
+++ spec/jobs/sentry_job_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe SentryJob, type: :job do
+  before { ActiveJob::Base.queue_adapter = :test }
+
+  describe '.perform' do
+    it do
+      expect { described_class.perform_later('event') }
+        .to have_enqueued_job
+        .on_queue('default')
+        .at(:no_wait)
+    end
+
+    it do
+      expect(Raven).to receive(:send_event).with('event')
+      described_class.perform_now('event')
+    end
+  end
+end
