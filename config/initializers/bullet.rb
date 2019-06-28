3c7e48486865ce5e24bd30ba4ae60556bca7c1c3
rkorzeniec <r.korzeniec@gmail.com>
2019-06-28 20:47:59 +0200

    initialise bullet

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,10 @@
+Rails.application.configure do
+  if Rails.env.development?
+    config.after_initialize do
+      Bullet.enable = true
+      Bullet.alert = true
+      Bullet.console = true
+      Bullet.add_footer = true
+    end
+  end
+end
