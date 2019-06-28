ff1bb07d054e411239b077fb7737a7b53764c431
rkorzeniec <r.korzeniec@gmail.com>
2019-06-28 21:40:22 +0200

    add low level caching to visited countries counter

--- app/services/dashboard/visited_countries_counter.rb
+++ app/services/dashboard/visited_countries_counter.rb
@@ -5,35 +5,51 @@ module Dashboard
     end
 
     def visited_world_countries_count
-      uniq_visited_count(visited_checkins)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins)
+      end
     end
 
     def visited_european_countries_count
-      uniq_visited_count(visited_checkins.european)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.european)
+      end
     end
 
     def visited_north_american_countries_count
-      uniq_visited_count(visited_checkins.north_american)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.north_american)
+      end
     end
 
     def visited_south_american_countries_count
-      uniq_visited_count(visited_checkins.south_american)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.south_american)
+      end
     end
 
     def visited_asian_countries_count
-      uniq_visited_count(visited_checkins.asian)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.asian)
+      end
     end
 
     def visited_oceanian_countries_count
-      uniq_visited_count(visited_checkins.oceanian)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.oceanian)
+      end
     end
 
     def visited_african_countries_count
-      uniq_visited_count(visited_checkins.african)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.african)
+      end
     end
 
     def visited_antarctican_countries_count
-      uniq_visited_count(visited_checkins.antarctican)
+      Rails.cache.fetch(cache_key(__method__), expires_in: 1.week) do
+        uniq_visited_count(visited_checkins.antarctican)
+      end
     end
 
     private
@@ -46,6 +62,15 @@ module Dashboard
 
     def uniq_visited_count(countries)
       Set.new(countries).size
+    end
+
+    def cache_key(method_name)
+      [
+        self.class.to_s.underscore,
+        method_name,
+        user.id,
+        user.visited_checkins.last&.id
+      ].compact.join('/')
     end
   end
 end
