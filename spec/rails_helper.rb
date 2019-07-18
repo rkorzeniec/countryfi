ce411b4c5981467a0df682d11d5fd0ab5c084967
rkorzeniec <r.korzeniec@gmail.com>
2019-07-18 20:23:10 +0200

    add factorybot to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include ApplicationHelper, type: :controller
+  config.include FactoryBot::Syntax::Methods
 
   config.around(:each, :migration) do |example|
     ActiveRecord::Migration.verbose = false
