191a4231a07d477e8eff694c15fa81f93aee04b3
Robert Korzeniec <r.korzeniec@gmail.com>
2020-04-25 13:20:36 +0200

    rspec github action

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.assets.precompile += [
+    'graphiql/rails/application.css',
+    'graphiql/rails/application.js'
+  ]
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
