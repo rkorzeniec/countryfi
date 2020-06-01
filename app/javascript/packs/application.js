8d67068bc9ffdb41687da4f1ff0ac0c71e0d8bb9
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-01 07:10:01 +0200

    bootstra

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
-import $ from 'jquery';
+// js
 import Rails from 'rails-ujs';
-import Turbolinks  from 'turbolinks';
+import Turbolinks from 'turbolinks';
+
+import('src/plugins');
+
+// css
+import 'css/site';
 
 window.$ = $;
 Rails.start();
