123622d6e6eb18ccf7ac506c214ff1ba0a1f5d00
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-01 15:36:16 +0200

    add dashboard PORO specs

---
+++ spec/dashboards/checkin_dashboard_spec.rb
@@ -0,0 +1,34 @@
+describe CheckinDashboard do
+  it do
+    expect(described_class::ATTRIBUTE_TYPES).to eq(
+      user: Administrate::BaseDashboard::Field::BelongsTo,
+      country: Administrate::BaseDashboard::Field::BelongsTo,
+      id: Administrate::BaseDashboard::Field::Number,
+      checkin_date: Administrate::BaseDashboard::Field::DateTime,
+      created_at: Administrate::BaseDashboard::Field::DateTime,
+      updated_at: Administrate::BaseDashboard::Field::DateTime
+    )
+  end
+
+  it do
+    expect(described_class::COLLECTION_ATTRIBUTES).to eq(
+      %i[id user country checkin_date].freeze
+    )
+  end
+
+  it do
+    expect(described_class::SHOW_PAGE_ATTRIBUTES).to eq(
+      %i[id user country checkin_date created_at updated_at].freeze
+    )
+  end
+
+  it do
+    expect(described_class::FORM_ATTRIBUTES).to eq(
+      %i[user country checkin_date].freeze
+    )
+  end
+
+  it do
+    expect(described_class::COLLECTION_FILTERS).to be_empty
+  end
+end
---
+++ spec/dashboards/currency_dashboard_spec.rb
@@ -0,0 +1,33 @@
+describe CurrencyDashboard do
+  it do
+    expect(described_class::ATTRIBUTE_TYPES).to eq(
+      country: Administrate::BaseDashboard::Field::BelongsTo,
+      id: Administrate::BaseDashboard::Field::Number,
+      code: Administrate::BaseDashboard::Field::String,
+      created_at: Administrate::BaseDashboard::Field::DateTime,
+      updated_at: Administrate::BaseDashboard::Field::DateTime
+    )
+  end
+
+  it do
+    expect(described_class::COLLECTION_ATTRIBUTES).to eq(
+      %i[id country code created_at].freeze
+    )
+  end
+
+  it do
+    expect(described_class::SHOW_PAGE_ATTRIBUTES).to eq(
+      %i[id country code created_at updated_at].freeze
+    )
+  end
+
+  it do
+    expect(described_class::FORM_ATTRIBUTES).to eq(
+      %i[country code].freeze
+    )
+  end
+
+  it do
+    expect(described_class::COLLECTION_FILTERS).to be_empty
+  end
+end
