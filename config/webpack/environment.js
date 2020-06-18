310a8baac44f7cab273ab4d1033c1b3086cf90e4
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-18 21:50:15 +0200

    append webpack plugins

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 
-environment.plugins.prepend(
+environment.plugins.append(
   "Provide",
   new webpack.ProvidePlugin({
     $: "jquery",
