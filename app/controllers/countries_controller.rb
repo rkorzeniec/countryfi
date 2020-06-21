c198a04773ef7ab8576f6e91b9b383f7b7a573d0
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-21 21:33:46 +0200

    fix rubocop violation

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,11 +7,11 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country
-               .preload(
-                 :country_languages, :currencies, :country_calling_codes,
-                 :top_level_domains, :country_alternative_spellings,
-                 border_countries: :border_country
-               ).find_by(cca2: params[:id])
+      .preload(
+        :country_languages, :currencies, :country_calling_codes,
+        :top_level_domains, :country_alternative_spellings,
+        border_countries: :border_country
+      ).find_by(cca2: params[:id])
     @geojson =
       File.read("app/assets/geojsons/#{country_cca3}.geo.json")
   end
