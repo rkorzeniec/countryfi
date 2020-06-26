44be23f0d23e0115b8faa5f0f2d7250ffd4876d2
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-26 22:26:40 +0200

    introduce calling codes updater

---
+++ spec/lib/countries/calling_codes_updater_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+describe Countries::CallingCodesUpdater do
+  let(:updater) { described_class.new(country: country, data: data) }
+  let(:country) { create(:country) }
+  let(:data) { %w[+41 +297] }
+
+  it do
+    expect(described_class::LOG_COLUMNS).to eq(%w[country_id code])
+  end
+
+  describe '#call' do
+    subject { updater.call }
+
+    let!(:calling_code) do
+      create(:country_calling_code, country: country, calling_code: '+41')
+    end
+
+    it 'updates records' do
+      expect(CountryCallingCode).to receive(:find_or_create_by)
+        .twice
+        .and_call_original
+
+      expect { subject }.to change { country.country_calling_codes.count }
+        .from(1).to(2)
+
+      calling_code = CountryCallingCode.first
+      expect(calling_code.calling_code).to eq('+41')
+
+      calling_code = CountryCallingCode.last
+      expect(calling_code.calling_code).to eq('+297')
+    end
+  end
+end
