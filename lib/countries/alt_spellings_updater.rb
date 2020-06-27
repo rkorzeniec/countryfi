e5efb61bb0f798e7111b2a1e2c9934bac930f0bf
Robert Korzeniec <r.korzeniec@gmail.com>
2020-06-27 09:53:00 +0200

    introduce alt spellings updater

---
+++ lib/countries/alt_spellings_updater.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Countries
+  class AltSpellingsUpdater
+
+    LOG_COLUMNS = %w[country_id name].freeze
+
+    def initialize(country:, data:)
+      @country = country
+      @data = data
+    end
+
+    def call
+      data.each do |spelling_data|
+        alt_spelling = find_or_create_by(spelling_data)
+        update_alt_spelling(alt_spelling, spelling_data)
+        log_update(alt_spelling)
+      end
+    end
+
+    private
+
+    attr_reader :country, :data
+
+    def find_or_create_by(alt_spelling)
+      CountryAlternativeSpelling.find_or_create_by(
+        country: country, name: alt_spelling
+      )
+    end
+
+    def update_alt_spelling(alt_spelling, data)
+      attrs = { name: data }
+      alt_spelling.update!(attrs)
+    end
+  end
+end
---
+++ spec/lib/countries/alt_spellings_updater_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+describe Countries::AltSpellingsUpdater do
+  let(:updater) { described_class.new(country: country, data: data) }
+  let(:country) { create(:country) }
+  let(:data) do
+    ['CH', 'Swiss Confederation', 'Schweiz', 'Suisse', 'Svizzera', 'Svizra']
+  end
+
+  it do
+    expect(described_class::LOG_COLUMNS).to eq(%w[country_id name])
+  end
+
+  describe '#call' do
+    subject { updater.call }
+
+    let!(:alt_spelling) do
+      create(:country_alternative_spelling, country: country, name: 'CH')
+    end
+
+    it 'updates records' do
+      expect(CountryAlternativeSpelling).to receive(:find_or_create_by)
+        .exactly(6).times
+        .and_call_original
+
+      expect { subject }.to change { country.country_alternative_spellings.count }
+        .from(1).to(6)
+
+      expect(country.country_alternative_spellings.map(&:name)).to eq(data)
+    end
+  end
+end
