177ecc3c27f7e7adc6ba294e30739d439a8ca65f
Robert Korzeniec <r.korzeniec@gmail.com>
2020-09-13 09:57:15 +0200

    user preferences form

---
+++ app/forms/users/preferences_form.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Users
+  class PreferencesForm
+    include ActiveModel::Validations
+
+    ATTRIBUTES = %i[countries color].freeze
+    COUNTRIES = {
+      all: 'all',
+      un: 'un_member',
+      independent: 'independent'
+    }.freeze
+
+    attr_accessor(*ATTRIBUTES)
+
+    validates :countries, presence: true, inclusion: { in: COUNTRIES.values }
+    validates :color,
+              format: { with: /\A#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})\z/ },
+              allow_nil: true
+
+    def initialize(user, params = {})
+      @user = user
+      ATTRIBUTES.each do |attribute|
+        send("#{attribute}=", params[attribute])
+      end
+    end
+
+    def save
+      valid? ? user.update(params) : false
+    end
+
+    def persisted?
+      false
+    end
+
+    private
+
+    attr_reader :user
+
+    def params
+      { color: color, countries_cluster: countries }
+    end
+  end
+end
