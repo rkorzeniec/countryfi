14acfbe9efd9581c1e0c3a7bb1035a7b09c9f2c5
Robert Korzeniec <r.korzeniec@gmail.com>
2020-10-15 19:56:11 +0200

    hide GQL introspection in production

--- app/graphql/countryfier_schema.rb
+++ app/graphql/countryfier_schema.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CountryfierSchema < GraphQL::Schema
+  disable_introspection_entry_points if Rails.env.production?
+
   query(Types::QueryType)
   mutation(Types::MutationType)
 end
