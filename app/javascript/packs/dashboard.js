8cfb0e9ff93c3204874e53f002b199c583ba225a
Robert Korzeniec <r.korzeniec@gmail.com>
2020-10-29 08:05:54 +0100

    remove easy pie chart from dashboard pack

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,11 +1,9 @@
 require('chartkick');
 require('chart.js');
 
-import EasyPieChart from 'easy-pie-chart';
 import svgPanZoom from 'svg-pan-zoom';
 import Hammer from 'hammerjs';
 
-window.EasyPieChart = EasyPieChart;
 window.svgPanZoom = svgPanZoom;
 window.ssag = require('save-svg-as-png');
 window.Hammer = Hammer;
