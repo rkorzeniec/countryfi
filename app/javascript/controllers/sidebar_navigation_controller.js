7665ed227adf0d9562a9e34e338a3add8243c012
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-07 11:02:33 +0100

    migrate sidebar navigation to stimulus

---
+++ app/javascript/controllers/sidebar_navigation_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['sidebar']
+
+   connect() {
+    const path = this.getURLPath()
+    const sidebarItem = this.sidebarTarget.querySelector('a[href*="' + path + '"]')
+    const sidebarSection = sidebarItem.closest('.sidebar-section')
+
+    sidebarItem.classList.add('active')
+    sidebarSection.querySelector('a').classList.add('active')
+    sidebarSection.querySelector('.sidebar-collapsable').classList.add('show')
+  }
+
+  getURLPath() {
+    return '/' + String(document.location).split('/').slice(-2).join('/').replace(/#.*$/, '')
+  }
+}
