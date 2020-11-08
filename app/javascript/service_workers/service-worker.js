93e87409d54f094df5f2ce2bc5a2b3bc6fb9bcd3
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-08 11:33:41 +0100

    add skeleton service worker

---
+++ app/javascript/service_workers/service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing. ');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activated. ');
+});
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker fetching. ');
+});
