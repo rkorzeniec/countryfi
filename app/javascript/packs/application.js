cc0da760fafc250c18083f7952504c70ae925e96
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-10 20:40:13 +0100

    move dom initialisaion to async module

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,28 +1,13 @@
 // JS inline imports
-import Rails from 'rails-ujs'
-import Turbolinks from 'turbolinks'
-import 'bootstrap'
+import Rails from "rails-ujs"
+import Turbolinks from "turbolinks"
+import "bootstrap"
 
 // JS async imports
-import('src/imports/application')
+import("src/imports/application")
 
 // styles imports
-import 'styles/site'
+import "styles/site"
 
-window.$ = $
 Rails.start()
 Turbolinks.start()
-
-// DOM events initialisation
-document.addEventListener("turbolinks:load", () => {
-  const worldSvgCountries = $('#world-svg-map [data-toggle="tooltip"]')
-  worldSvgCountries.tooltip({
-    container: 'body',
-    placement: 'right',
-    trigger: 'hover',
-    template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>'
-  })
-
-  worldSvgCountries.on('mousedown', (e) => $(e.currentTarget).tooltip('hide'))
-  worldSvgCountries.on('mouseup', (e) => $(e.currentTarget).tooltip('show'))
-})
