87761ce078b4bfd49183dcd7ae664f49710ce2eb
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-12 20:42:54 +0100

    use stimulusJS for yearly countries chart

---
+++ app/services/dashboard/yearly_countries_chart_decorator.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Dashboard
+  class YearlyCountriesChartDecorator
+    include CacheFetch
+
+    def initialize(user)
+      @user = user
+    end
+
+    def labels
+      cache_fetch(__method__) do
+        all_visited_countries.keys | unique_visited_countries.keys
+      end
+    end
+
+    def series
+      cache_fetch(__method__) do
+        [all_visited_countries.values, unique_visited_countries.values]
+      end
+    end
+
+    private
+
+    attr_reader :user
+
+    def all_visited_countries
+      @all_visited_countries ||= VisitedCountriesQuery.new(user).count_by_year
+    end
+
+    def unique_visited_countries
+      @unique_visited_countries ||= UniqVisitedCountriesQuery.new(user).count_by_year
+    end
+  end
+end
---
+++ spec/services/dashboard/yearly_countries_chart_decorator_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+describe Dashboard::YearlyCountriesChartDecorator do
+  let(:decorator) { described_class.new(user) }
+  let(:user) { build_stubbed(:user) }
+
+  let(:unique_query) do
+    instance_double(
+      UniqVisitedCountriesQuery,
+      count_by_year: { 1992 => 1, 2001 => 2, 2005 => 1, 2020 => 5 }
+    )
+  end
+  let(:all_query) do
+    instance_double(
+      VisitedCountriesQuery,
+      count_by_year: { 1992 => 2, 2000 => 4, 2019 => 2, 2020 => 10 }
+    )
+  end
+
+  shared_context 'with cached method' do
+    let(:cache) { instance_double('cache') }
+    let(:cache_key) do
+      [
+        'dashboard/yearly_countries_chart_decorator',
+        method_name,
+        user.cache_key
+      ].join('/')
+    end
+
+    it do
+      expect(Rails).to receive(:cache).and_return(cache)
+      expect(cache).to receive(:fetch).with(cache_key, expires_in: 1.week)
+      subject
+    end
+  end
+
+  describe '#labels' do
+    subject { decorator.labels }
+
+    it_behaves_like 'with cached method' do
+      let(:method_name) { 'labels' }
+    end
+
+    it do
+      expect(VisitedCountriesQuery).to receive(:new)
+        .with(user).and_return(all_query)
+      expect(UniqVisitedCountriesQuery).to receive(:new)
+        .with(user).and_return(unique_query)
+
+      is_expected.to include(1992, 2000, 2001, 2005, 2019, 2020)
+    end
+  end
+
+  describe '#series' do
+    subject { decorator.series }
+
+    it_behaves_like 'with cached method' do
+      let(:method_name) { 'series' }
+    end
+
+    it do
+      expect(VisitedCountriesQuery).to receive(:new)
+        .with(user).and_return(all_query)
+      expect(UniqVisitedCountriesQuery).to receive(:new)
+        .with(user).and_return(unique_query)
+
+      is_expected.to eq([[2, 4, 2, 10], [1, 2, 1, 5]])
+    end
+  end
+end
