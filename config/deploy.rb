b3ebad1f302dbe3c4e8db4fa04b172cf9236f0e2
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-12 19:10:15 +0100

    set delayed job capistrano config for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,6 @@ set :local_user, 'deploy'
 
 set :keep_releases, 5
 
-set :delayed_job_workers, 1
-
 append :linked_dirs,
        'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
        'public/system', 'public/uploads'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,7 @@ server '78.47.203.108', user: 'deploy', roles: %w[web app db]
 set :branch, 'master'
 set :conditionally_migrate, true
 
+set :delayed_job_workers, 1
+set :delayed_job_service, -> { 'delayed_job' }
+
 after 'deploy:published', 'delayed_job:restart'
