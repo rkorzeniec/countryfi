5fa10f2d4741b18bba638a84210a360233cbbd33
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-13 21:16:25 +0100

    set SameSite Lax option to cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store(
-  :cookie_store, key: '_countrify_session', httponly: true
+  :cookie_store, key: '_countrify_session', httponly: true, same_site: :lax
 )
