d4eb263fa49f57bb50bdb58a2a9a1c99d46f35c8
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-21 20:26:10 +0100

    unturbolinks tolltip event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 // JS inline imports
 import Rails from 'rails-ujs'
 import Turbolinks from 'turbolinks'
-import 'jquery'
-import 'bootstrap/js/dist/util'
-import 'bootstrap/js/dist/tooltip'
 
 // styles imports
 import 'styles/site'
@@ -13,19 +10,3 @@ import('src/imports/application')
 
 Rails.start()
 Turbolinks.start()
-
-/* eslint-disable no-undef */
-// DOM events initialisation
-document.addEventListener('turbolinks:load', () => {
-  const worldSvgCountries = $("#world-svg-map [data-toggle='tooltip']")
-  worldSvgCountries.tooltip({
-    container: 'body',
-    placement: 'right',
-    trigger: 'hover',
-    template: "<div class='tooltip' role='tooltip'><div class='tooltip-inner'></div></div>"
-  })
-
-  worldSvgCountries.on('mousedown', (e) => $(e.currentTarget).tooltip('hide'))
-  worldSvgCountries.on('mouseup', (e) => $(e.currentTarget).tooltip('show'))
-})
-/* eslint-enable no-undef */
--- app/javascript/src/imports/application.js
+++ app/javascript/src/imports/application.js
@@ -1,2 +1,19 @@
 import 'popper.js'
 import 'controllers'
+import 'jquery'
+import 'bootstrap/js/dist/util'
+import 'bootstrap/js/dist/tooltip'
+
+/* eslint-disable no-undef */
+// DOM events initialisation
+const worldSvgCountries = $("#world-svg-map [data-toggle='tooltip']")
+worldSvgCountries.tooltip({
+  container: 'body',
+  placement: 'right',
+  trigger: 'hover',
+  template: "<div class='tooltip' role='tooltip'><div class='tooltip-inner'></div></div>"
+})
+
+worldSvgCountries.on('mousedown', (e) => $(e.currentTarget).tooltip('hide'))
+worldSvgCountries.on('mouseup', (e) => $(e.currentTarget).tooltip('show'))
+/* eslint-enable no-undef */
