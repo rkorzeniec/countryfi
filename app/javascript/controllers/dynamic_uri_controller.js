1edde75493c60e5e250443501bf1a176a414d112
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-28 19:02:41 +0100

    allow user to copy profile URL

--- app/javascript/controllers/dynamic_uri_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'stimulus'
-
-export default class extends Controller {
-  static targets = ['result']
-
-  update(event) {
-    this.resultTarget.innerHTML = this.data.get('url') + event.target.value
-  }
-}
