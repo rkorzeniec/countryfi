76cf611de39aa8d53d9502959011698aab1b2d68
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-30 07:37:31 +0100

    better flash JS handling

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = ['element']
+
+  initialize() {
+    setTimeout(() => {
+      this.elementTarget.classList.toggle('show')
+      setTimeout(() => this.elementTarget.remove(), 250)
+    }, this.data.get('value'));
+  }
+
+  close() {
+    this.elementTarget.remove()
+  }
+}
