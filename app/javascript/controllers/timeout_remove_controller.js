96d9e6329ccf9bfb8e9bce13c0e792717d8c1cbe
Robert Korzeniec <r.korzeniec@gmail.com>
2020-11-30 07:00:21 +0100

    better flash message timeout naming

--- app/javascript/controllers/timeout_remove_controller.js
+++ app/javascript/controllers/timeout_remove_controller.js
@@ -4,6 +4,6 @@ export default class extends Controller {
   static targets = ['element']
 
   initialize() {
-    setTimeout(() => this.elementTarget.remove(), 3000);
+    setTimeout(() => this.elementTarget.remove(), this.data.get('value'));
   }
 }
