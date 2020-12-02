35a46a0d9ebcee3c97c0c94e481110987b41864b
Robert Korzeniec <r.korzeniec@gmail.com>
2020-12-02 06:16:08 +0100

    move service worker registration to dynamic import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,24 +7,3 @@ import 'styles/site'
 
 Rails.start()
 Turbolinks.start()
-
-// initialise service workers
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration)
-
-    let serviceWorker
-    if (registration.installing) {
-      serviceWorker = registration.installing
-      console.log('Service worker installing.')
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting
-      console.log('Service worker installed & waiting.')
-    } else if (registration.active) {
-      serviceWorker = registration.active
-      console.log('Service worker active.')
-    }
-  }).catch(registrationError => {
-    console.log('Service worker registration failed: ', registrationError)
-  })
-})
