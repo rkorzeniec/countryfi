4356e3944899ac68f598ff37313185125cf56c5b
Robert Korzeniec <r.korzeniec@gmail.com>
2020-12-02 06:16:34 +0100

    service worker available check

--- app/javascript/packs/bootstrap.js
+++ app/javascript/packs/bootstrap.js
@@ -23,22 +23,24 @@ $('#overlay').modal('show')
 /* eslint-enable no-undef */
 
 // initialise service workers
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registered: ', registration)
 
-    let serviceWorker
-    if (registration.installing) {
-      serviceWorker = registration.installing
-      console.log('Service worker installing.')
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting
-      console.log('Service worker installed & waiting.')
-    } else if (registration.active) {
-      serviceWorker = registration.active
-      console.log('Service worker active.')
-    }
-  }).catch(registrationError => {
-    console.log('Service worker registration failed: ', registrationError)
+      let serviceWorker
+      if (registration.installing) {
+        serviceWorker = registration.installing
+        console.log('Service worker installing.')
+      } else if (registration.waiting) {
+        serviceWorker = registration.waiting
+        console.log('Service worker installed & waiting.')
+      } else if (registration.active) {
+        serviceWorker = registration.active
+        console.log('Service worker active.')
+      }
+    }).catch(registrationError => {
+      console.log('Service worker registration failed: ', registrationError)
+    })
   })
-})
+}
