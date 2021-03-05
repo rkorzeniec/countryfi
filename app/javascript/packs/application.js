ff399a2ac59a2af9bb05fe2df2db0eebe467d400
Robert Korzeniec <r.korzeniec@gmail.com>
2021-03-05 07:24:14 +0100

    Move service worker initialisation to application JS pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,14 @@ import 'styles/site'
 
 Rails.start()
 Turbolinks.start()
+
+// initialize service workers
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(registration => {
+      console.log('[Service worker] Registered: ', registration)
+    }).catch(registrationError => {
+      console.log('[Service worker] Registration failed: ', registrationError)
+    })
+  })
+}
--- app/javascript/packs/bootstrap.js
+++ app/javascript/packs/bootstrap.js
@@ -21,26 +21,3 @@ worldSvgCountries.on('mouseup', (e) => $(e.currentTarget).tooltip('show'))
 
 $('#overlay').modal('show')
 /* eslint-enable no-undef */
-
-// initialise service workers
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('ServiceWorker registered: ', registration)
-
-      let serviceWorker
-      if (registration.installing) {
-        serviceWorker = registration.installing
-        console.log('Service worker installing.')
-      } else if (registration.waiting) {
-        serviceWorker = registration.waiting
-        console.log('Service worker installed & waiting.')
-      } else if (registration.active) {
-        serviceWorker = registration.active
-        console.log('Service worker active.')
-      }
-    }).catch(registrationError => {
-      console.log('Service worker registration failed: ', registrationError)
-    })
-  })
-}
