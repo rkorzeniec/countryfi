238b0abbda65e36d4d372add5634f356a9fd65f4
Robert Korzeniec <r.korzeniec@gmail.com>
2021-03-07 14:01:49 +0100

    Improve service worker script

--- app/javascript/src/service_worker.js
+++ app/javascript/src/service_worker.js
@@ -1,11 +1,9 @@
 console.log('ServiceWorker imported')
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(registration => {
-      console.log('[Service worker] Registered: ', registration)
-    }).catch(registrationError => {
-      console.log('[Service worker] Registration failed: ', registrationError)
-    })
+  navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(registration => {
+    console.log('[Service worker] Registered: ', registration)
+  }).catch(registrationError => {
+    console.log('[Service worker] Registration failed: ', registrationError)
   })
 }
