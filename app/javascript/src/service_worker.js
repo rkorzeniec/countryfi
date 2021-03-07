e9d3abf1af944ab67efd3730957fe72b27bbe6cd
Robert Korzeniec <r.korzeniec@gmail.com>
2021-03-07 14:50:36 +0100

    Comment out service worker console logs

--- app/javascript/src/service_worker.js
+++ app/javascript/src/service_worker.js
@@ -2,8 +2,8 @@ console.log('ServiceWorker imported')
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(registration => {
-    console.log('[Service worker] Registered: ', registration)
+    // console.log('[Service worker] Registered: ', registration)
   }).catch(registrationError => {
-    console.log('[Service worker] Registration failed: ', registrationError)
+    // console.log('[Service worker] Registration failed: ', registrationError)
   })
 }
