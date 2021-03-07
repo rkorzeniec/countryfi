b5f1011d891abe5cfc75e0d77137b76ecde4f5a1
Robert Korzeniec <r.korzeniec@gmail.com>
2021-03-07 13:39:51 +0100

    Skip layout for service worker controller

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ServiceWorkerController < ApplicationController
+  layout false
+
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
 
