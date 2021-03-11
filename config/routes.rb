d1a464575219e9c755ce6af4f74d7471c97de395
Robert Korzeniec <r.korzeniec@gmail.com>
2021-03-11 06:45:22 +0100

    Rearrange routes
    In order to use devise's routing "authenticated" helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,85 +1,89 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post '/graphql', to: 'graphql#execute'
-
-  namespace :profile do
-    get '(:profile_name)', action: :show
-    resource :availability, only: %i[show]
-  end
-
-  devise_for :users
-
-  resources :countries, only: :show
-
-  resources :checkins, except: %i[index show]
-
-  namespace :checkins do
-    resources :worlds, only: :index
-    resources :europeans, only: :index
-    resources :north_americans, only: :index
-    resources :south_americans, only: :index
-    resources :asians, only: :index
-    resources :africans, only: :index
-    resources :oceanians, only: :index
-    resources :antarcticans, only: :index
-  end
-
-  namespace :explore do
-    resources :worlds, only: :index
-    resources :europes, only: :index
-    resources :north_americas, only: :index
-    resources :south_americas, only: :index
-    resources :asias, only: :index
-    resources :africas, only: :index
-    resources :oceanias, only: :index
-    resources :antarcticas, only: :index
-  end
-
-  resource :preferences, only: %i[edit update]
-
-  resources :notifications, only: :index do
-    post :mark_as_read, on: :collection
-    post :mark_as_read, on: :member
-  end
-
-  get 'about', to: 'about#index'
-  get 'terms', to: 'terms#index'
   root to: 'hello#index'
-
   # FIXME: once webpacker is able to support service workers
   get '/service-worker.js', to: 'service_worker#service_worker'
   get '/manifest.json', to: 'service_worker#manifest'
 
+  post '/graphql', to: 'graphql#execute'
+
+  devise_for :users
+
+  authenticated :user do
+    root to: 'profile#show', as: :root_app
+
+    namespace :profile do
+      get '(:profile_name)', action: :show
+      resource :availability, only: %i[show]
+    end
+
+    resources :checkins, except: %i[index show]
+
+    namespace :checkins do
+      resources :worlds, only: :index
+      resources :europeans, only: :index
+      resources :north_americans, only: :index
+      resources :south_americans, only: :index
+      resources :asians, only: :index
+      resources :africans, only: :index
+      resources :oceanians, only: :index
+      resources :antarcticans, only: :index
+    end
+
+    namespace :explore do
+      resources :worlds, only: :index
+      resources :europes, only: :index
+      resources :north_americas, only: :index
+      resources :south_americas, only: :index
+      resources :asias, only: :index
+      resources :africas, only: :index
+      resources :oceanias, only: :index
+      resources :antarcticas, only: :index
+    end
+
+    resource :preferences, only: %i[edit update]
+
+    resources :notifications, only: :index do
+      post :mark_as_read, on: :collection
+      post :mark_as_read, on: :member
+    end
+
+    namespace :admin do
+      resources :users
+      resources :border_countries
+      resources :checkins
+      resources :countries
+      resources :announcements
+      resources :notifications
+      resources :country_alternative_spellings
+      resources :country_calling_codes
+      resources :country_languages
+      resources :currencies
+      resources :top_level_domains
+
+      root to: 'users#index'
+    end
+  end
+
+  resources :countries, only: :show
+
+  get 'about', to: 'about#index'
+  get 'terms', to: 'terms#index'
+
   get 'shell', to: 'shell#index'
   get 'offline', to: 'offline#index'
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-  end
 
   get '/404', to: 'exceptions#index', code: '404'
   get '/422', to: 'exceptions#index', code: '422'
   get '/500', to: 'exceptions#index', code: '500'
 
-  namespace :admin do
-    resources :users
-    resources :border_countries
-    resources :checkins
-    resources :countries
-    resources :announcements
-    resources :notifications
-    resources :country_alternative_spellings
-    resources :country_calling_codes
-    resources :country_languages
-    resources :currencies
-    resources :top_level_domains
-
-    root to: 'users#index'
-  end
-
   # TODO: remove after a while, because of the switch to profiles
   get 'dashboard', to: redirect('/profile')
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
 
   unless Rails.application.config.consider_all_requests_local
     get '*path', to: 'exceptions#index', code: '404'
