a6293444b10b219f88821a096c25e006df23828f
Robert Korzeniec <r.korzeniec@gmail.com>
2021-11-11 21:20:35 +0100

    Enable bullet in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Bullet.alert         = true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -68,4 +70,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
