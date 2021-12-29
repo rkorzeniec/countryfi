a75b8004fd4d01dc87a6b13ebf38a4ebcbb382ef
Robert Korzeniec <r.korzeniec@gmail.com>
2021-12-29 12:12:12 +0100

    Keep less deployment releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:rkorzeniec/countryfier.git'
 set :deploy_to, "/var/www/html/apps/#{fetch(:application)}"
 set :local_user, 'deploy'
 
-set :keep_releases, 5
+set :keep_releases, 3
 
 append :linked_dirs,
        'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
