7272eea83774f8d652a08634bedfc64490f38208
Robert Korzeniec <r.korzeniec@gmail.com>
2022-12-09 21:22:53 +0100

    Add hotwire js packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,7 @@ pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.1
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'tailwindcss-stimulus-components' # @3.0.4
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+pin '@hotwired/turbo-rails', to: '@hotwired--turbo-rails.js' # @7.2.4
+pin '@hotwired/turbo', to: '@hotwired--turbo.js' # @7.2.4
+pin '@rails/actioncable/src', to: '@rails--actioncable--src.js' # @7.0.4
