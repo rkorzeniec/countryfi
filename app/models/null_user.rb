48106722ca524e5aef577a5e8cab49ac53f1f8f3
Robert Korzeniec <r.korzeniec@gmail.com>
2022-12-10 13:45:58 +0100

    Add null user

---
+++ app/models/null_user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class NullUser
+  def unread_notifications
+    []
+  end
+end
---
+++ spec/models/null_user_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+describe NullUser do
+  let(:user) { described_class.new }
+
+  describe '#unread_notifications' do
+    subject { user.unread_notifications }
+
+    it { is_expected.to be_empty }
+  end
+end
